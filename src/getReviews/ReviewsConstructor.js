c697da0372e3378c0aa4be8ecde0d0a7535f3162
Aquafresh <deathsnow22@gmail.com>
2016-05-03 19:53:35 +0300

    change review and gallery module on prototype type

--- src/getReviews/ReviewsConstructor.js
+++ src/getReviews/ReviewsConstructor.js
@@ -8,29 +8,34 @@ var getReviewBlock = require('./getReviewBlock');
  * @constructor
  */
 function Review(data, container) {
-
   this.data = data;
   this.element = getReviewBlock(this.data);
-  var currentElement = this.element;
+  this.currentElement = this.element;
 
-  this.onReviewClick = function(event) {
-    var answerNegative = currentElement.querySelector('.review-quiz-answer-no');
-    var answerPositive = currentElement.querySelector('.review-quiz-answer-yes');
-
-    if(event.target.classList.contains('review-quiz-answer')) {
-      answerNegative.classList.remove('review-quiz-answer-active');
-      answerPositive.classList.remove('review-quiz-answer-active');
-      event.target.classList.add('review-quiz-answer-active');
-    }
-  };
-
-  this.remove = function() {
-    this.element.removeEventListener('click', this.onReviewClick);
-    this.element.parentNode.removeChild(this.element);
-  };
+  this.remove = this.remove.bind(this);
+  this.onReviewClick = this.onReviewClick.bind(this);
 
   container.appendChild(this.element);
   this.element.addEventListener('click', this.onReviewClick);
 }
+
+Review.prototype.remove = function() {
+  this.element.removeEventListener('click', this.onReviewClick);
+  this.element.parentNode.removeChild(this.element);
+};
+
+/**
+ * @param  {Event} event
+ */
+Review.prototype.onReviewClick = function(event) {
+  this.answerNegative = this.currentElement.querySelector('.review-quiz-answer-no');
+  this.answerPositive = this.currentElement.querySelector('.review-quiz-answer-yes');
+
+  if(event.target.classList.contains('review-quiz-answer')) {
+    this.answerNegative.classList.remove('review-quiz-answer-active');
+    this.answerPositive.classList.remove('review-quiz-answer-active');
+    event.target.classList.add('review-quiz-answer-active');
+  }
+};
 
 module.exports = Review;
